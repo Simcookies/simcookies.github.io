7a2b77b8c110f796bcf895d3fd150528d0cc57ff
Song <mssongan840136@outlook.com>
2017-03-20 11:55:56 +0900

    Update at 2017-03-20 02:55:56 UTC

--- public/script/main.js
+++ public/script/main.js
@@ -1,44 +1,53 @@
 jQuery(document).ready(function() {
 
-    // quick button
-    var topBtn = jQuery('#quick-link');
-    topBtn.hide();
-    jQuery(window).scroll(function () {
-	if (jQuery(this).scrollTop() > 100) {
-	    topBtn.fadeIn();
-	} else {
-	    topBtn.fadeOut();
-	}
-    });
-    jQuery('#page-top').click(function () {
-	jQuery('body,html').animate({
-	    scrollTop: 0
-	}, 300);
-	return false;
-    });
-
-    // menu-icon button
-    var menuBtn = jQuery('#menu-icon');
-    var toggleMenu = menuBtn.next('nav').children('.trigger');
-    menuBtn.click(function() {
-	toggleMenu.toggle('fast');
-	return false;
-    });
-
-    if(menuBtn.css('display') == "block") {
-	jQuery(document).click(function() {
-	    toggleMenu.hide('fast');
-	});
-
-	toggleMenu.click(function(e) {
-	    e.stopPropagation();
-	});
+  // quick button
+  var topBtn = jQuery('#quick-link');
+  topBtn.hide();
+  jQuery('.scroll-content').scroll(function () {
+    if (jQuery(this).scrollTop() > 100) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
     }
+  });
 
-    //TOC-area
-    var markBtn = jQuery("#bookmark");
-    markBtn.click(function () {
-	jQuery("article").toggleClass("home post", "fast", "easeOutSine");
-	jQuery("#toc-area").toggle("50");
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > 100) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+
+  jQuery('#page-top').click(function () {
+    jQuery('.scroll-content,body,html').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
+
+  // menu-icon button
+  var menuBtn = jQuery('#menu-icon');
+  var toggleMenu = menuBtn.next('nav').children('.trigger');
+  menuBtn.click(function() {
+    toggleMenu.toggle('fast');
+    return false;
+  });
+
+
+  jQuery(document).click(function() {
+    if(menuBtn.css('display') == "block") {
+      toggleMenu.hide('fast');
+    }
+    toggleMenu.click(function(e) {
+      e.stopPropagation();
     });
+  });
+
+  //TOC-area
+  var markBtn = jQuery("#bookmark");
+  markBtn.click(function () {
+    jQuery("article").toggleClass("home post", "fast", "easeOutSine");
+    jQuery("#toc-area").toggle("50");
+  });
 });
